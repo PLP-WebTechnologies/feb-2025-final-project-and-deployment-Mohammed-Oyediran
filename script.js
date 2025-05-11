1b37fa77c6e67afa1216b97e7e4e8ca5a58fc3f8
Mohammed <95849758+oyediranmohammed@users.noreply.github.com>
2025-05-11 15:04:35 +0400

    Completed project

--- script.js
+++ script.js
@@ -1,70 +1,62 @@
-const navs = document.querySelectorAll('.nav-list li');
-const cube = document.querySelector('.box');
-const sections = document.querySelectorAll('.section');
+const navs = document.querySelectorAll(".nav-list li a");
+const sections = document.querySelectorAll(".section");
 
+const resumeLists = document.querySelectorAll(".resume-list");
+const resumeBoxs = document.querySelectorAll(".resume-box");
 
-const resumeLists = document.querySelectorAll('.resume-list');
-const resumeBoxs = document.querySelectorAll('.resume-box');
+const portfolioLists = document.querySelectorAll(".portfolio-list");
+const portfolioBoxs = document.querySelectorAll(".portfolio-box");
 
-const portfolioLists = document.querySelectorAll('.portfolio-list');
-const portfolioBoxs = document.querySelectorAll('.portfolio-box');
-
-// navbar actions and all section actions along with cube rotation when navbar is clicked
+// navbar actions and all section actions when navbar is clicked
 navs.forEach((nav, idx) => {
-    nav.addEventListener('click', () => {
-        document.querySelector('.nav-list li.active').classList.remove('active');
-        nav.classList.add('active');
+  nav.addEventListener("click", () => {
+    document.querySelector(".nav-list li.active").classList.remove("active");
+    nav.classList.add("active");
 
+    document.querySelector(".section.active").classList.remove("active");
+    sections[idx].classList.add("active");
 
-        cube.style.transform = `rotateY(${idx * -90}deg)`;
+    const array = Array.from(sections);
+    const arrSecs = array.slice(1, -1); //only requires indexes 1, 2, 3 or does not require start and end indexes
+    arrSecs.forEach((arrSec) => {
+      if (arrSec.classList.contains("active")) {
+        sections[4].classList.add("action-contact");
+      }
 
-        document.querySelector('.section.active').classList.remove('active');
-        sections[idx].classList.add('active');
-        
-        const array = Array.from(sections);
-        const arrSecs = array.slice(1, -1); //only requires indexes 1, 2, 3 or does not require start and end indexes
-        arrSecs.forEach(arrSec => {
-            if (arrSec.classList.contains('active')) {
-                sections[4].classList.add('action-contact');
-            }
-
-            if(sections[0].classList.contains('active')) {
-                sections[4].classList.remove('action-contact');
-            }
-        })
+      if (sections[0].classList.contains("active")) {
+        sections[4].classList.remove("action-contact");
+      }
     });
+  });
 });
-
 
 // resume section when clicking tab-list
 resumeLists.forEach((list, idx) => {
-    list.addEventListener('click', () => {
-        document.querySelector('.resume-list.active').classList.remove('active');
-        list.classList.add('active');
+  list.addEventListener("click", () => {
+    document.querySelector(".resume-list.active").classList.remove("active");
+    list.classList.add("active");
 
-        document.querySelector('.resume-box.active').classList.remove('active');
-        resumeBoxs[idx].classList.add('active');
-        
-    });
+    document.querySelector(".resume-box.active").classList.remove("active");
+    resumeBoxs[idx].classList.add("active");
+  });
 });
-
 
 // portfolio section when clicking tab-list
 
-
 portfolioLists.forEach((list, idx) => {
-    list.addEventListener('click', () => {
-        document.querySelector('.portfolio-list.active').classList.remove('active');
-        list.classList.add('active');   
-        
-        
-        document.querySelector('.portfolio-box.active').classList.remove('active');
-        portfolioBoxs[idx].classList.add('active');
-    });
+  list.addEventListener("click", () => {
+    document.querySelector(".portfolio-list.active").classList.remove("active");
+    list.classList.add("active");
+
+    document.querySelector(".portfolio-box.active").classList.remove("active");
+    portfolioBoxs[idx].classList.add("active");
+  });
 });
 
-//visibility for contact section when reloading (cube reloading animation)
+//hamburger menu
+const toggle = document.querySelector(".menu-toggle");
+const navList = document.querySelector(".nav-list");
 
-setTimeout(()=> {
-    sections[4].classList.remove('active')
-}, 1500);
+toggle.addEventListener("click", () => {
+  navList.classList.toggle("show");
+});
